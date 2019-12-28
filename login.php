58053c69b38f496bc6539996d2cdfe187da165e7
sifat <sifat.ezzyr@gmail.com>
2019-12-28 14:23:34 +0600

    html upload

---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, shrink-to-fit=9">
+		<meta name="description" content="Gambolthemes">
+		<meta name="author" content="Gambolthemes">
+		<title>Ticket Gripe - Login Now</title>
+		
+		<!-- Favicon Icon -->
+		<link rel="icon" type="image/png" href="images/fav.png">
+		
+		<!-- Stylesheets -->
+		<link href="css/responsive.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/datepicker.min.css" rel="stylesheet">
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+		<link href="vendor/OwlCarousel/assets/owl.carousel.css" rel="stylesheet">
+		<link href="vendor/OwlCarousel/assets/owl.theme.default.min.css" rel="stylesheet">
+	
+	</head>
+
+	<body class="body-bg" oncontextmenu="return false;">				
+		<!-- Body Start -->	
+		<main class="register-mp">	
+			<div class="main-section">
+				<div class="container">					
+					<div class="row justify-content-md-center">
+						<div class="col-md-10">
+							<div class="login-register-bg">
+								<div class="row no-gutters">
+									<div class="col-lg-6">
+										<div class="lg-left">
+											<div class="lg-logo">
+												<a href="index.php"><img src="images/login-register/logo.svg" alt=""></a>
+											</div>
+											<div class="lr-text">
+												<h2>Login Now</h2>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum blandit felis a hendrerit.</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6">
+										<div class="lr-right">
+										<h4>Sign In to Ticket Gripe</h4>
+											<div class="login-register-form">
+												<form>
+													<div class="form-group">									
+														<input class="title-discussion-input" type="email" name="email" placeholder="Type Email or Phone Number" required>
+													</div>
+													<div class="form-group">									
+														<input class="title-discussion-input" type="password" name="password" placeholder="Password" required>
+													</div>
+													<button class="login-btn" type="submit">Login Now</button>
+												</form>
+												<a href="#" class="forgot-link">Forgot Password?</a>
+												<div class="regstr-link">Don’t have an account? <a href="register.php">Register Now</a></div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>							
+					</div>										
+										
+				</div>
+			</div>
+		</main>
+		<!-- Body End -->			
+		<?php include('master_layout/footer.php') ?>
---
+++ register.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, shrink-to-fit=9">
+		<meta name="description" content="Gambolthemes">
+		<meta name="author" content="Gambolthemes">
+		<title>Ticket Gripe - Register Now</title>
+		
+		<!-- Favicon Icon -->
+		<link rel="icon" type="image/png" href="images/fav.png">
+		
+		<!-- Stylesheets -->
+		<link href="css/responsive.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/datepicker.min.css" rel="stylesheet">
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+		<link href="vendor/OwlCarousel/assets/owl.carousel.css" rel="stylesheet">
+		<link href="vendor/OwlCarousel/assets/owl.theme.default.min.css" rel="stylesheet">
+	
+	</head>
+
+	<body class="body-bg" oncontextmenu="return false;">				
+		<!-- Body Start -->	
+		<main class="register-mp">	
+			<div class="main-section">
+				<div class="container">				
+					<div class="row justify-content-md-center">
+						<div class="col-md-10">
+							<div class="login-register-bg">
+								<div class="row no-gutters">
+									<div class="col-lg-6">
+										<div class="lg-left">
+											<div class="lg-logo">
+												<a href="index.html"><img src="images/login-register/logo.svg" alt=""></a>
+											</div>
+											<div class="lr-text">
+												<h2>Register Now</h2>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum blandit felis a hendrerit.</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6">
+										<div class="lr-right">
+											<h4>Sign Up to Ticket Gripe</h4>
+											<div class="login-register-form">
+												<form>
+													<div class="form-group">									
+														<input class="title-discussion-input" type="text" name="name" placeholder="Full Name" required>
+													</div>
+													<div class="form-group">									
+														<input class="title-discussion-input" type="email" name="email" placeholder="Email Address" required>
+													</div>
+													<div class="form-group">								 	
+														<input class="title-discussion-input" type="password" name="password" placeholder="Password" required>
+													</div>													
+													<div class="rgstr-dt-txt">
+														By clicking Sign Up, you agree to our <a href="#">Terms</a>, <a href="#">Data Policy</a> and <a href="#">Cookie Policy</a>. You may receive Email notifications from us and can opt out at any time.
+													</div>
+													<button class="login-btn" type="submit">Register Now</button>
+													<div class="login-link">If you have an account? <a href="login.php">Login Now</a></div>
+												</form>											
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>							
+					</div>										
+										
+				</div>
+			</div>
+		</main>
+		<!-- Body End -->			
+		<?php include('master_layout/footer.php') ?>
