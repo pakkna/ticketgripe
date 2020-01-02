0afe70ab7f0c3f50d8ca00503bd5c19205d82c67
sifat <sifat.ezzyr@gmail.com>
2020-01-02 14:38:02 +0600

    nothing

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::group(['middleware' => 'auth'], function () {
     //my-events page
     Route::get("user-setting","HomeController@user_setting")->name("UserSetting");
     Route::post("basic-info","HomeController@edit_basic_info")->name("BasicInfo");
-    Route::post("basic-info","HomeController@edit_user_avatar")->name("UserAvatarCng");
+    Route::post("basic-info2","HomeController@edit_user_avatar")->name("UserAvatarCng");
     Route::get("my-events","EventControler@show_my_events")->name("MyEvents");
     Route::get("add-events","EventControler@show_event_form")->name("AddEvent");
     Route::post("insert-event","EventControler@create_event")->name("createEvent");
